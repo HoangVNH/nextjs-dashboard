8b623163e76741fc215789d88ff3bce577a94dd8
Hoang, Vu <hoang.vu@zoi.tech>
2025-01-27 23:23:02 +0700

    add new files

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -135,6 +135,7 @@ export async function fetchInvoicesPages(query: string) {
     const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
     return totalPages;
   } catch (error) {
+    console.log("Failed to fetch total number of invoices.");
     console.error("Database Error:", error);
     throw new Error("Failed to fetch total number of invoices.");
   }
